e74636ea05ee56b442284b9749588490dc1302c0
ivgakatcher <itavr.working@gmail.com>
2024-04-03 16:58:21 +0300

    some

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите количество участников: ");
-        int pers = scanner.nextInt();
         System.out.println("Введите грузоподъёмность байдарок: ");
         int cargo = scanner.nextInt();
-
-        System.out.println("Hello world!");
+        inputCount() ;
     }
-    public static int[] inputData(){
+    public static int[] inputCount(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите количество участников: ");
         int number = scanner.nextInt();
-        System.out.println("Введите грузоподъёмность байдарок: ");
-        int cargo = scanner.nextInt();
+        int [] kayakerWeight = new int[];
+        System.out.println("Введите вес участников: ");
+        for ( int i = 0; i < number; i++) {
+            kayakerWeight[i] = scanner.nextInt();
+        }
+        return kayakerWeight;
     }
-    //Done: input initial data
-    //TODO: write "procedure" for analyze and output certain data
+    public static int orderBoats(int[] kayakerWeight){
+        int n = kayakerWeight.length;
+        int sumWeight = 0;
+        for ( int i = 0; i < n; i++) {
+            if ()
+        }
+    }
 }
